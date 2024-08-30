f8231a3683324782315f60b69c6fa899c8ca1bac
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-31 01:55:08 +0600

    feat: Add lazy loading for images and update homepage layout

--- app/components/category/Category.js
+++ app/components/category/Category.js
@@ -1,4 +1,5 @@
 import { getCategory } from '@/app/action/category';
+import Image from 'next/image';
 import React from 'react'
 
 const Category = async () => {
@@ -6,16 +7,25 @@ const Category = async () => {
     const category = await getCategory();
 
     return (
-        <div className="flex space-x-5">
+        <div>
+            <div className="text-center text-[#007E85] text-3xl font-semibold">Services we provide</div>
+            <div className="text-base mt-0.5 font-semibold text-center text-white">Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. </div>
+            {/* <div className="flex justify-between">
             {
                 category?.map((cat) => {
                     return (
                         <div key={cat._id}>
+                            <div className="bg-[#D6F2F2] flex justify-center items-center p-5 rounded-full">
+
+                            <Image  style={{maxHeight:50,minHeight:50,maxWidth:50,minWidth:50}} src={cat.icon} alt={cat.title} width={20} height={20} />
+                            </div>
+
                             <h1>{cat.title}</h1>
                         </div>
                     )
                 })
             }
+            </div> */}
         </div>
     )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["plus.unsplash.com" ,"motionview.com.bd",'m.media-amazon.com',"adminapi.applegadgetsbd.com",'www.applegadgetsbd.com', 'adminapi.applegadgetsbd.com', 'images.unsplash.com', 'lh3.googleusercontent.com', 'i.imgur.com'],
+    },
+    serverActions: {
+      bodySizeLimit: '2mb' // Set desired value here
+    }
+  };
+  
+  export default nextConfig;
