1b128e593a4475427a6e1f13bc22f6ff546a01c7
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-30 22:35:10 +0600

    chore: Add header component to layout and adjust body styling

--- app/page.js
+++ app/page.js
@@ -1,13 +1,10 @@
 
 import Banner from "./components/Banner";
-import Header from "./components/header/Header";
+
 
 export default function Home() {
   return (
     <main className="common-class">
-      <div className="mb-[120px]">
-         <Header/>
-      </div>
       <Banner/>
     </main>
   );
