4f970f55336ea21ed76a9dbee55239e1f34a0beb
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-30 16:28:10 +0600

    chore: Refactor Banner component and update content

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -4,18 +4,46 @@ import React from "react";
 
 const Banner = () => {
   return (
-  <div className="flex flex-col items-center justify-between md:flex-row">
-  <div className="mb-4 text-center md:text-left md:mb-0">
-    Providing Quality Healthcare for a Brighter and Healthy Future
-  </div>
-  <div className="relative">
-    <Image src={iVector} alt="Banner" className="w-[80%]" />
-    <div className="absolute top-[-60px] w-full md:w-auto">
-      <Image src={iBannerDoctor} alt="Banner" className="w-[80%] l" />
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="flex flex-col justify-center text-center md:text-left">
+        <h1 className="text-lg font-semibold">
+          Providing Quality Healthcare for a Brighter and Healthy Future
+        </h1>
+        <p className="mt-2">
+          At our hospital, we are dedicated to providing exceptional medical
+          care to our patients and their families. Our experienced team of
+          medical professionals, cutting-edge technology, and compassionate
+          approach make us a leader in the healthcare industry.
+        </p>
+      </div>
+
+      <div className="relative flex items-center justify-center md:items-end md:justify-end">
+        <Image src={iVector} alt="Background Vector" className="w-[60%]" />
+        <div className="absolute top-[-40px] md:right-[40px] w-[50%]">
+          <Image src={iBannerDoctor} alt="Doctor" />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
 
 export default Banner;
+
+
+
+{/* <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
+  <div className="text-center md:text-left">
+    <h2 className="mb-4">
+      Providing Quality Healthcare for a Brighter and Healthy Future
+    </h2>
+    <p>
+      At our hospital, we are dedicated to providing exceptional medical care to our patients and their families. Our experienced team of medical professionals, cutting-edge technology, and compassionate approach make us a leader in the healthcare industry.
+    </p>
+  </div>
+  <div className="relative flex justify-center md:justify-end">
+    <Image src={iVector} alt="Banner" className="w-3/4 md:w-1/2 lg:w-1/3" />
+    <div className="absolute top-[-60px] w-3/4 md:w-1/2 lg:w-1/3">
+      <Image src={iBannerDoctor} alt="Banner" className="w-full" />
+    </div>
+  </div>
+</div> */}
