7665d03d4c8d37275442a3933d422829205174cf
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-31 09:39:36 +0600

    Update Category component layout and add professionals image to homepage banner

--- app/components/category/Category.js
+++ app/components/category/Category.js
@@ -9,23 +9,19 @@ const Category = async () => {
     return (
         <div>
             <div className="text-center text-[#007E85] text-3xl font-semibold">Services we provide</div>
-            <div className="text-base mt-0.5 font-semibold text-center text-white">Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. </div>
-            {/* <div className="flex justify-between">
-            {
-                category?.map((cat) => {
-                    return (
-                        <div key={cat._id}>
-                            <div className="bg-[#D6F2F2] flex justify-center items-center p-5 rounded-full">
-
-                            <Image  style={{maxHeight:50,minHeight:50,maxWidth:50,minWidth:50}} src={cat.icon} alt={cat.title} width={20} height={20} />
-                            </div>
-
-                            <h1>{cat.title}</h1>
-                        </div>
-                    )
-                })
-            }
-            </div> */}
+            <div className="text-base mt-0.5 mb-5 font-semibold text-center text-white">Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac tellus libero accumsan. </div>
+            <div className="flex flex-wrap justify-between gap-y-5">
+                {
+                    category?.map((cat) => {
+                        return (
+                                <div key={cat._id} className="flex flex-col max-w-[250px] min-w-[250px] items-center justify-center p-5 bg-white rounded">
+                                    <div className="w-full h-44 bg-[#D6F2F2] "></div>
+                                    <h1 className="text-black">{cat.title}</h1>
+                                </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
