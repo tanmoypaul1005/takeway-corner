3107f90a3a975abbe4b45b4d63260afef624f920
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-01 23:05:32 +0600

    Update Testimonial model schema and add timestamps

---
+++ models/Testimonial.ts
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const testimonialSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    image: {
+        type: String,
+        default: ""
+    },
+    designation: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+  comment: {
+        type: String,
+        required: true,
+        trim: true
+    },
+},{timestamps:true});
+
+export default mongoose?.models?.Testimonial || mongoose?.model('Testimonial', testimonialSchema);
+
