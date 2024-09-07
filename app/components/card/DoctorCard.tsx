6993f2a636039054ee57a8c1a830196d8bdb3aba
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-07 10:14:04 +0600

    feat: Update DoctorCard component to display doctor details
    The DoctorCard component has been updated to display the details of a doctor, including their specialization, name, qualifications, and ratings. The component now receives the doctor object as a prop and uses the corresponding properties to populate the UI elements. This ensures that the correct information is shown for each doctor card.

--- app/components/card/DoctorCard.tsx
+++ app/components/card/DoctorCard.tsx
@@ -2,36 +2,37 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { iDummyDoctor } from "../../../util/imageImports";
+import CommonRating from "../ratting/CommonRating";
 
-export const DoctorCard = async () => {
-  
+export const DoctorCard = async ({ doctor }) => {
+
   return (
-    <Link href={`/products/`}>
+    <Link href={`/doctor/${doctor?._id}`}>
       <div className="relative w-64 py-5 text-white duration-500 bg-gray-800 shadow-md sm:w-full bg-cCommonBg rounded-xl hover:scale-105 hover:shadow-xl">
         <Image
           style={{ minHeight: "120px", maxHeight: "120px" }}
           alt={"Product Image"}
           width={400}
           height={500}
-          src={iDummyDoctor}
+          src={doctor?.image ?? iDummyDoctor}
           className="object-contain object-center w-full h-32 rounded-t-xl"
         />
 
         <div className="px-3 pt-2">
-          <span className="mr-3 text-xs uppercase">
-          Consultant - Radiology & Imaging
+          <span className="mr-3 text-[10px] uppercase">
+            {doctor?.specialization}
           </span>
-          <p className="block text-sm font-semibold capitalize truncate line-clamp-2">
-            Dr. Shoheli Sultana
+          <p className="block text-base font-semibold capitalize truncate line-clamp-2">
+            {doctor?.name}
           </p>
-          {/* <div className="my-2">
-            <CommonRating value={parseInt(product?.ratting ?? 0)} />
-          </div> */}
+          <div className="my-2">
+            <CommonRating value={parseInt(doctor?.ratings ?? 0)} />
+          </div>
           <div className="flex justify-between">
-            <p className="text-base font-semibold text-red-400 cursor-auto">
-              {/* ${product?.price ?? 0} */} MBBS, MD
+            <p className="text-sm font-semibold text-red-400 cursor-auto line-clamp-2">
+              {doctor?.qualifications?.map((qualification) => qualification).join(", ")}
             </p>
-{/* 
+            {/* 
             {session?.user?.email && (
               <ProductCardAction
                 data={{
@@ -44,8 +45,8 @@ export const DoctorCard = async () => {
           </div>
         </div>
         <div className="absolute top-0.5 right-0.5">
-       {/* {product?.offer?.offerPercentage && <span className="px-2 py-1 text-xs font-semibold text-white bg-red-500 rounded-full">{product?.offer?.offerPercentage}% OFF</span>} */}
-      </div>
+          {/* {product?.offer?.offerPercentage && <span className="px-2 py-1 text-xs font-semibold text-white bg-red-500 rounded-full">{product?.offer?.offerPercentage}% OFF</span>} */}
+        </div>
       </div>
     </Link>
   );
--- app/doctor/page.tsx
+++ app/doctor/page.tsx
@@ -10,7 +10,7 @@ const Doctor = async() => {
         <section className="grid grid-cols-1 gap-4 p-4 rounded bg-slate-900 place-self-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {
             doctors.map((doctor) => (
-              <DoctorCard key={doctor._id} />))
+              <DoctorCard key={doctor._id} doctor={doctor} />))
           }
       </section>
     )
