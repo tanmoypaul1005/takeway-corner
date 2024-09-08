6fadfcd8c903b8427b07360c54d49f1040f83118
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-08 22:49:59 +0600

    refactor: Update AppointmentModal and tailwind.config.js
    Refactor AppointmentModal component to use new CSS classes for styling and update tailwind.config.js to add new colors. These changes improve the visual appearance and functionality of the component.

--- app/doctor/modal/AppointmentModal.tsx
+++ app/doctor/modal/AppointmentModal.tsx
@@ -1,8 +1,8 @@
-"use client"
-import React from 'react';
-import CommonModal from '../../components/modal/ComonModal';
-import CommonInput from '../../components/input/CommonInput';
-import CommonDatePicker from '../../components/input/CommonDatePicker';
+"use client";
+import React from "react";
+import CommonModal from "../../components/modal/ComonModal";
+import CommonInput from "../../components/input/CommonInput";
+import CommonDatePicker from "../../components/input/CommonDatePicker";
 
 export interface TimeSlot {
   start: string;
@@ -16,38 +16,56 @@ interface AppointmentModalProps {
   selectDoctor?: TimeSlot[];
 }
 
-const AppointmentModal: React.FC<AppointmentModalProps> = ({ open, setOpen,selectDoctor }) => {
-
-  console.log("selectDoctor", selectDoctor);
+const AppointmentModal: React.FC<AppointmentModalProps> = ({
+  open,
+  setOpen,
+  selectDoctor,
+}) => {
   
+
   return (
     <div>
       <CommonModal
-        setOpen={setOpen} 
-        open={open} 
-        content={<>
-        <div>
-          <div className='text-3xl font-bold text-center text-white'>Book Appointment</div>
-          <div className='text-center text-white'>Please enter your name to book an appointment</div>
-        </div>
-        <form className='flex flex-col mb-5 gap-y-5'>
-            <CommonInput placeHolder='Enter your Name' level='Name'/>
-            <CommonInput placeHolder='Enter your Email' level='Email'/>
-            <CommonInput placeHolder='Enter your Phone' level='Phone'/>
-            {/* <CommonDatePicker/> */}
-        </form>
-        <div className='flex flex-wrap gap-3'>
-
-        {selectDoctor?.map((timeSlot: TimeSlot, index: number) => (
-          <div key={index} className='flex bg-[#007E85] text-sm rounded-md px-4 py-2  justify-between text-white'>
-            {timeSlot.start} - {timeSlot.end}
-        </div>
-      ))}
-      </div>
-        </>}
+        setOpen={setOpen}
+        open={open}
+        content={
+          <>
+            <div>
+              <div className="text-3xl font-bold text-center text-white">
+                Book Appointment
+              </div>
+              <div className="text-center text-white">
+                Please enter your name to book an appointment
+              </div>
+            </div>
+            <form className="flex flex-col mb-5 gap-y-5">
+              <CommonInput placeHolder="Enter your Name" level="Name" />
+              <CommonInput placeHolder="Enter your Email" level="Email" />
+              <CommonInput placeHolder="Enter your Phone" level="Phone" />
+              {/* <CommonDatePicker/> */}
+            </form>
+            <div className="flex flex-wrap gap-3">
+              {selectDoctor?.map((timeSlot: TimeSlot, index: number) => (
+                <div
+                  key={index}
+                  className={`flex max-w-[120px] min-w-[120px] justify-center items-center py-2 text-sm text-white rounded-md ${
+                    timeSlot.available
+                      ? "bg-cPrimary"
+                      : "bg-gray-400 cursor-not-allowed"
+                  }`}
+                  style={{
+                    pointerEvents: timeSlot.available ? "auto" : "none",
+                  }}
+                >
+                  {timeSlot.start} - {timeSlot.end} 
+                </div>
+              ))}
+            </div>
+          </>
+        }
       />
     </div>
   );
-}
+};
 
 export default AppointmentModal;
