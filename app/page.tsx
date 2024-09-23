7dc858cdb574536638c92e5ab7afe5d389f8434e
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-23 23:57:14 +0600

    Refactor import paths in page.tsx and tsconfig.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-// import Appointment from "./appointment/page";
-// import Banner from "./components/banner/Banner";
-// import TestimonialSlider from "./components/testimonials/TestimonialSlider";
-// import Doctor from "./doctor/page";
-
 import Banner from "@app/components/banner/Banner";
 import Appointment from "@app/appointment/page";
 import Doctor from "@app/doctor/page";
