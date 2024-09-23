a8023b9bf94f9830e7b7897aa9d7ad42e2dedddb
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-09-23 22:44:16 +0600

    Refactor doctor slots route.ts and CommonDatePicker.tsx

--- app/doctor/modal/AppointmentModal.tsx
+++ app/doctor/modal/AppointmentModal.tsx
@@ -5,7 +5,9 @@ import CommonInput from "../../components/input/CommonInput";
 import CommonDatePicker from "../../components/input/CommonDatePicker";
 import Image from "next/image";
 import { iCheck } from "../../../util/imageImports";
-import useSWR from 'swr'
+
+// Define the fetcher function
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export interface TimeSlot {
   start: string;
@@ -24,10 +26,10 @@ const AppointmentModal: React.FC<AppointmentModalProps> = ({
   open,
   setOpen,
   selectDoctor,
-  doctor_id
 }) => {
 
   
+
   const [selectedIndex, setSelectedIndex] = useState(null);
 
   const [formValues, setFormValues] = useState({
