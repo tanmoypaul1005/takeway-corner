0c170648e39f71c7942aa060388637a0a8e62e0d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-05 21:30:28 +0600

    Refactor import paths in Banner component and CommonDatePicker component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import Banner from "@app/components/banner/Banner";
 import Appointment from "@app/appointment/page";
-import Doctor from "@app/doctor/page";
 import TestimonialSlider from "@app/components/testimonials/TestimonialSlider";
-
 
 const Home: React.FC = () => {
   return (
   <main className="space-y-10">
       <Banner/>
       <Appointment/>
-      <Doctor/>
       <TestimonialSlider/>
       <div className="mb-10"></div>
     </main>
