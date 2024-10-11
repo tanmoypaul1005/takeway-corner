3cd9c75b4c31ffe382966558ffe3178506c403ce
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-11 11:28:18 +0600

    Refactor import paths and update CuisinesCard and Shop components

--- app/cuisines/page.tsx
+++ app/cuisines/page.tsx
@@ -9,11 +9,15 @@ const Cuisines = () => {
         Your favourite cuisines
       </div>
 
-      <div>
-<CuisinesCard/>
-<CuisinesCard/>
-<CuisinesCard/>
-<CuisinesCard/>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7">
+        <CuisinesCard title="Biryani" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/193.png?height=192" />
+        <CuisinesCard title="Pizza" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/73.png?height=192" />
+        <CuisinesCard title="Burgers" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/85.png?height=192" />
+        <CuisinesCard title="Cakes" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/77.png?height=192" />
+        <CuisinesCard title="Breakfast" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/193.png?height=192" />
+        <CuisinesCard title="Pasta" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/73.png?height=192" />
+        <CuisinesCard title="Khichuri" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/85.png?height=192" />
+        <CuisinesCard title="Cafe" img="https://images.deliveryhero.io/image/foodpanda/cuisine-images/BD/77.png?height=192" />
       </div>
     </div>
   );
