d632bef9a79902c5884e09825aa24ecfa46a0d5d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-11 19:59:10 +0600

    Refactor import paths and update restaurant page with popular dishes

--- app/restaurant/page.tsx
+++ app/restaurant/page.tsx
@@ -7,17 +7,6 @@ const Restaurant = () => {
       <div className="">
         <div className="text-[32px] font-semibold leading-10 text-white mb-5">
           Sweet tooth 🔥
-          <ShopCard/>
-          <ShopCard />
-          <ShopCard />
-          <ShopCard />
-        </div>
-      </div>
-
-
-      <div className="">
-        <div className="text-[32px] font-semibold leading-10 text-white mb-5">
-        Gorom gorom biryani 🔥
         </div>
         <div className="grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <ShopCard />
@@ -27,10 +16,21 @@ const Restaurant = () => {
         </div>
       </div>
 
+      <div className="">
+        <div className="text-[32px] font-semibold leading-10 text-white mb-5">
+          Gorom gorom biryani 🔥
+        </div>
+        <div className="grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <ShopCard />
+          <ShopCard />
+          <ShopCard />
+          <ShopCard />
+        </div>
+      </div>
 
       <div className="">
         <div className="text-[32px] font-semibold leading-10 text-white mb-5">
-        Homechefs 🔥
+          Homechefs 🔥
         </div>
         <div className="grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <ShopCard />
