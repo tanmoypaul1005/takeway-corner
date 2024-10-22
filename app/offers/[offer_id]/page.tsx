9d93c0f2a30f6562a95aa7f4897db56e66207860
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-22 20:47:11 +0600

    Refactor OfferDetails page to include offer banner image

--- app/offers/[offer_id]/page.tsx
+++ app/offers/[offer_id]/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { fetchOfferDetails } from "@app/action/offer";
+import Image from "next/image";
+import React from "react";
 
-const OfferDetails = () => {
-    return (
-        <div>
-            ,kjbkl
-        </div>
-    )
-}
+const OfferDetails = async ({ params }) => {
+  const offerDetails = await fetchOfferDetails(params.offer_id);
 
-export default OfferDetails
+  return (
+    <div className="flex items-center justify-center">
+      <Image
+        src={offerDetails?.banner}
+        style={{
+          maxWidth: "80%",
+          minWidth: "80%",
+          maxHeight: "320px",
+          minHeight: "320px",
+        }}
+        alt="offer banner"
+        width={2000}
+        height={2000}
+        className="w-fit h-fit"
+      />
+    </div>
+  );
+};
+
+export default OfferDetails;
