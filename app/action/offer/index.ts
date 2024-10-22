270e0d05e63ed56f3ce00c9e64415261e3285238
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-22 20:04:02 +0600

    Refactor fetchOffer function to include offer details and shop information

--- app/action/offer/index.ts
+++ app/action/offer/index.ts
@@ -1,11 +1,33 @@
 import Offer from "models/Offer";
+import Shop from "models/Shop";
 import connectMongo from "util/connectMongo";
 
-export const fetchOffer = async (id) => {
+export const fetchOffer = async () => {
     try {
         await connectMongo();
-        const food = await Offer.find({})
-        return food;
+        const offer = await Offer.find({})
+        return offer;
+    } catch (e) {
+     return null
+    }
+}
+
+
+export const fetchOfferDetails = async (id) => {
+    try {
+        await connectMongo();
+        const offer = await Offer.findOne({_id:id});
+
+
+        // Fetch category details
+        const productDetails = await Promise.all(
+            await offer.products.map(async (categoryId) => {
+                const products = await Shop.findOne({_id:categoryId});
+                return products;
+            })
+        );
+
+        return { ...offer._doc, shop: productDetails };
     } catch (e) {
      return null
     }
