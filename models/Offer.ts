634959939d6bab81489bfa74c14c01a0cbc04fbb
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-22 18:51:32 +0600

    Refactor Offer model to remove unnecessary title and description fields

--- models/Offer.ts
+++ models/Offer.ts
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose');
 
 const offerSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      description: {
-        type: String,
-        required: true,
-        trim: true
-      },
+  
       discountPercentage: {
         type: Number,
         required: true,
