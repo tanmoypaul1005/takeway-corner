9492d898e0bb38b4f65c416b1e5502a35702d34a
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-26 18:42:49 +0600

    Update dependencies and add bcrypt for password hashing

--- util/utilityFunctions.ts
+++ util/utilityFunctions.ts
@@ -1,5 +1,6 @@
 import { toast, ToastOptions, TypeOptions } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import bcrypt from 'bcrypt';
 
 interface ToastrProps {
   message?: string;
@@ -18,3 +19,10 @@ export const Toastr = ({ message = "", type = "error" }: ToastrProps) => {
     type: type,
   } as ToastOptions);
 };
+
+
+
+export async function verifyPassword(password, hashedPassword) {
+  const isValid = await bcrypt.compare(password, hashedPassword);
+  return isValid;
+}
