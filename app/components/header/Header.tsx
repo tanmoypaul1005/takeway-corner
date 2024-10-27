1ad253f9b28e622d21fd335ce977a17879e3aac5
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-10-27 13:08:04 +0600

    Refactor Google component to add onClick event handler for Google authentication

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,28 +4,46 @@ import { iLogo } from "../../../util/imageImports";
 import MobileNav from "./MobileNav";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import LogoutButton from "./LogoutButton";
+import UserProfile from "./UserProfile";
 
 const Header: React.FC = () => {
   const { data: session, status } = useSession();
-  console.log("session",session)
+  console.log("session", session);
   return (
     <div className="py-2 shadow-md shadow-cyan-500/50">
       <div className="flex justify-between common-class">
+        <Link href="/">
+          <Image src={iLogo} alt="Logo" />
+        </Link>
 
-      <Link href="/"><Image src={iLogo} alt="Logo" /></Link>
-
-      <div className="items-center justify-center hidden space-x-10 text-xl font-semibold md:flex">
-        <div className="cursor-pointer">Home</div>
-        <div className="cursor-pointer">Service</div>
-        <div className="cursor-pointer">Contact us</div>
-        <div className="cursor-pointer">Help</div>
-        <div className="cursor-pointer">Blogs</div>
-        <Link href="/register" className="cursor-pointer">Sign up</Link>
-        <Link href="/login" className="cursor-pointer">Sign in</Link>
-      </div>
-      <div className="flex md:hidden">
-        <MobileNav />
-      </div>
+        <div className="items-center justify-center hidden space-x-10 text-xl font-semibold md:flex">
+          <div className="cursor-pointer whitespace-nowrap">Home</div>
+          <div className="cursor-pointer whitespace-nowrap">Service</div>
+          <div className="cursor-pointer whitespace-nowrap">Contact us</div>
+          <div className="cursor-pointer whitespace-nowrap">Help</div>
+          <div className="cursor-pointer whitespace-nowrap">Blogs</div>
+          {session?.user ? (
+            <>
+<UserProfile session={session}/>
+            </>
+          ) : (
+            <>
+              <Link
+                href="/register"
+                className="cursor-pointer whitespace-nowrap"
+              >
+                Sign up
+              </Link>
+              <Link href="/login" className="cursor-pointer whitespace-nowrap">
+                Sign in
+              </Link>
+            </>
+          )}
+        </div>
+        <div className="flex md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </div>
   );
---
+++ app/components/header/UserProfile.tsx
@@ -0,0 +1,48 @@
+"use client"
+import { Toastr } from "@util/utilityFunctions";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { IoIosLogOut } from "react-icons/io";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const UserProfile = ({ session }) => {
+
+    const router=useRouter();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        Toastr({ message: "Logout successful!", type: "success" });
+        signOut({ callbackUrl: '/login' }); // Redirect to login page after logout
+    };
+
+  return (
+    <div>
+      <div 
+      onClick={(e)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        router.push("/profile")
+      }}
+      className="cursor-pointer whitespace-nowrap">
+        <div className="flex space-x-2">
+          <Image
+            width={30}
+            height={30}
+            src={session?.user.image}
+            alt="Profile"
+            className="rounded-full"
+          />
+          <div>{session?.user.name}</div>
+          <button onClick={handleLogout}>
+                <IoIosLogOut className='w-7 h-7'/>
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
